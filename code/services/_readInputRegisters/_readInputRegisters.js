3a4e22338481af053f10f6f5bb29d39d900c1c9d
Yash Jain <yashkjain1994@gmail.com>
2018-06-20 11:27:31 -0500

    Asset created by yashjain28

---
+++ code/services/_readInputRegisters/_readInputRegisters.js
@@ -0,0 +1,19 @@
+function _readInputRegisters(req, resp){
+    ClearBlade.init({request:req});
+    _req=req;
+    _resp=resp;
+	if (ClearBlade.isEdge()) {
+    	g_deleteMessages(MODBUSREQUESTTOPIC);
+        var msg = ClearBlade.Messaging();
+        msg.publish(MODBUSREQUESTTOPIC,JSON.stringify({
+            ModbusHost: MBHOST,
+            FunctionCode: MBFUNCTION.READINPUTREGISTERS,
+            UnitID: DEFAULTUNIT,
+            StartAddress: STARTADDR,
+            AddressCount: ENDADDR,
+            Data: []}));
+        resp.success(MODBUSREQUESTTOPIC);
+	} else
+	resp.success("Code can only execute on an edge");
+	
+}

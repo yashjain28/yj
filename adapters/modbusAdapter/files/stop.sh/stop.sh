824b12bd693e11c93bf2c926a3cb389c48df75cb
Yash Jain <yashkjain1994@gmail.com>
2018-06-20 11:27:19 -0500

    Asset created by yashjain28

---
+++ adapters/modbusAdapter/files/stop.sh/stop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then 
+  	echo "---------Permissions Error---------"
+  	echo "STOPPING: Please run as root or sudo"
+  	echo "-----------------------------------"
+  exit
+fi
+
+SCRIPTDIR="$( cd "$(dirname "$0")" ; pwd -P )"
+CONFIGFILENAME="adapterconfig.txt"
+source "$SCRIPTDIR/$CONFIGFILENAME"
+
+systemctl stop $ADAPTERSERVICENAME
